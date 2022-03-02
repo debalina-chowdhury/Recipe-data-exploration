15158447a915dc4834dff3bc3f8670ecd89df4b6
Selwyn Gomes <selwyngomes96@gmail.com>
2022-03-02 02:11:51 -0800

    Added new visualizations

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+import numpy as np
+from collections import Counter
 
 def make_plots(data,title,x='date',y='calories',z='line',time='year'):
     """Function takes a dataframe and the column names to make a plot of x v/s y. where x is datatime.
@@ -9,17 +12,19 @@ def make_plots(data,title,x='date',y='calories',z='line',time='year'):
     assert isinstance(data,pd.DataFrame)
     assert x in data.columns.values.tolist()
     assert y in data.columns.values.tolist()
-    assert z in ['line','bar']
+    assert z in ['line','bar','box']
+    sns.set()
     fig, axs = plt.subplots(figsize=(12, 4))
     if(time=='year'):
-        data.groupby(data[x].dt.year)[y].mean().plot(
-    kind=z, rot=0, ax=axs)
+        data.groupby(data[x].dt.year)[y].mean().plot(kind=z, rot=0, ax=axs)
+        plt.xticks(range(2002,2017))
+        axs.set_xlim([2002,2017])
+     
     elif(time=='month'):
-        data.groupby(data[x].dt.month)[y].mean().plot(
-    kind=z, rot=0, ax=axs)
+        data.groupby(data[x].dt.month)[y].mean().plot(kind=z, rot=0, ax=axs,xticks=np.arange(0,12))
+        axs.set_xticklabels(["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])
     else:
-        data.groupby(data[x].dt.day)[y].mean().plot(
-    kind=z, rot=0, ax=axs)
+        data.groupby(data[x].dt.day)[y].mean().plot(kind=z, rot=0, ax=axs)
     plt.title(title)
     plt.xlabel(time)
     plt.ylabel(y)
@@ -38,3 +43,36 @@ def extract_features(df,col,new_cols):
         df[new_cols[i]]=df[col].str[i]
         df[new_cols[i]]=pd.to_numeric(df[new_cols[i]])
     return(df)
+
+def popular_ingredients(df,year):
+    """
+    Takes a dataframe and year and returns a counter with the counts of all ingredients consumed in the year
+    """
+    assert year>=df['year'].min() and year<=df['year'].max()
+    assert isinstance(df,pd.DataFrame)
+    df2=df[(df.year == year)]
+    series=df2['ingredients']
+    series=series.tolist()
+    count = Counter()
+    for i in range(len(series)):
+        count.update(series[i])
+    return(count)
+
+def count_ingredients(df,name):
+    """
+    counts ingredients over all years and returns a list (normalised over the year)
+    
+    """
+    assert isinstance(df,pd.DataFrame)
+    year=df['year'].min()
+    y=[]
+    while year<=df['year'].max():
+        denom=len(df[df['year'] == year])
+        count=popular_ingredients(df,year)
+        sums=0
+        for key in count.keys():
+            if name in key:
+                sums+=count[key]
+        y.append(sums/denom)
+        year+=1
+    return(y) 
